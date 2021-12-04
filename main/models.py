15a4fc4d856b67c40700488aba3a6c9c42fbd724
Gabriel <gabriel@gomide.org>
2021-12-04 08:53:52 -0300

    No need for birthdays

--- main/models.py
+++ main/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class People(models.Model):
     name = models.CharField(max_length=200)
-    birthday = models.DateField(auto_now=False, auto_now_add=False)
+    birthday = models.DateField(auto_now=False, auto_now_add=False, null=True)
     email = models.CharField(max_length=100)
     number = models.CharField(max_length=50)
     instagram = models.CharField(max_length=100)
@@ -18,6 +18,7 @@ class People(models.Model):
 class Note(models.Model):
     title = models.CharField(max_length=200)
     notes = models.CharField(max_length=10000)
+    date = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.title
